631b82b0a070c4f4b3d0a25dbc35ba4b7ab65b57
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-01-03 19:06:12 +0530

    Note: Folder restructuring

--- backend/_Cpp/main.py
+++
@@ -1,9 +0,0 @@
-import StackModule
-
-stack = StackModule.Stack()
-stack.push(10)
-stack.push(20)
-print(stack.to_string())  # Output: [10, 20]
-print(stack.pop())        # Output: 20
-print(stack.is_empty())   # Output: False
-print(stack.top())        # Output: 10
